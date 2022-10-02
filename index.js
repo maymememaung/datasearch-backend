3c1542e18b8db9c6593ad217189ac2679b0f746d
maymememaung <73875703+maymememaung@users.noreply.github.com>
2022-10-02 01:45:38 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.get('/user', function (req, res) {
+    res.send('This is user page.')
+})
+
+module.exports.handler = serverless(app);
