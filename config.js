a01488a08cba76963d9365904bf73408f4e32233
maymememaung <73875703+maymememaung@users.noreply.github.com>
2022-10-02 16:51:37 -0400

    updated export

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-export const user = "maymaung"
-export const host = "datasearch-database-1-instance-1-us-east-1c.c8mot3egztv2.us-east-1.rds.amazonaws.com"
-export const db = "postgres"
-export const password = "B83IB99GFQCYT1M144an"
+const user = "maymaung"
+const host = "datasearch-database-1-instance-1-us-east-1c.c8mot3egztv2.us-east-1.rds.amazonaws.com"
+const db = "postgres"
+const password = "B83IB99GFQCYT1M144an"
+
+module.exports = {
+    user, host, db, password
+}
